df30e852d099812c38d79fc5ee8c5429ca8239cc
ABHISHEK KUMAR K P <abhishekkumarkp11a@gmail.com>
2024-12-20 20:05:07 +0530

    Update 8-BIT ALU.v

--- 8-BIT ALU/8-BIT ALU.v
+++ 8-BIT ALU/8-BIT ALU.v
@@ -15,20 +15,20 @@ endmodule
 module eight_bit_adder(
     input [7:0] A,
     input [7:0] B,
-    input Bin,
-    output [7:0] D,
-    output Bout
+    input Cin,
+    output [7:0] S,
+    output Cout
     );
     
     wire c1,c2,c3,c4,c5,c6,c7;
-    full_adder m0(A[0],B[0],Bin,D[0],c1);
-    full_adder m1(A[1],B[1],c1,D[1],c2);
-    full_adder m2(A[2],B[2],c2,D[2],c3);
-    full_adder m3(A[3],B[3],c3,D[3],c4);
-    full_adder m4(A[4],B[4],c4,D[4],c5);
-    full_adder m5(A[5],B[5],c5,D[5],c6);
-    full_adder m6(A[6],B[6],c6,D[6],c7);
-    full_adder m7(A[7],B[7],c7,D[7],Bout);
+    full_adder m0(A[0],B[0],Cin,S[0],c1);
+    full_adder m1(A[1],B[1],c1,S[1],c2);
+    full_adder m2(A[2],B[2],c2,S[2],c3);
+    full_adder m3(A[3],B[3],c3,S[3],c4);
+    full_adder m4(A[4],B[4],c4,S[4],c5);
+    full_adder m5(A[5],B[5],c5,S[5],c6);
+    full_adder m6(A[6],B[6],c6,S[6],c7);
+    full_adder m7(A[7],B[7],c7,S[7],Cout);
 endmodule
     
 module full_sub(
@@ -45,20 +45,20 @@ endmodule
 module eight_bit_sub(
     input [7:0] A,
     input [7:0] B,
-    input Cin,
-    output [7:0] S,
-    output Cout
+    input Bin,
+    output [7:0] D,
+    output Bout
     );
     
     wire c1,c2,c3,c4,c5,c6,c7;
-    full_sub m0(A[0],B[0],Cin,S[0],c1);
-    full_sub m1(A[1],B[1],c1,S[1],c2);
-    full_sub m2(A[2],B[2],c2,S[2],c3);
-    full_sub m3(A[3],B[3],c3,S[3],c4);
-    full_sub m4(A[4],B[4],c4,S[4],c5);
-    full_sub m5(A[5],B[5],c5,S[5],c6);
-    full_sub m6(A[6],B[6],c6,S[6],c7);
-    full_sub m7(A[7],B[7],c7,S[7],Cout);
+    full_sub m0(A[0],B[0],Bin,D[0],c1);
+    full_sub m1(A[1],B[1],c1,D[1],c2);
+    full_sub m2(A[2],B[2],c2,D[2],c3);
+    full_sub m3(A[3],B[3],c3,D[3],c4);
+    full_sub m4(A[4],B[4],c4,D[4],c5);
+    full_sub m5(A[5],B[5],c5,D[5],c6);
+    full_sub m6(A[6],B[6],c6,D[6],c7);
+    full_sub m7(A[7],B[7],c7,D[7],Bout);
 endmodule
 
 module eight_bit_multiplier(
